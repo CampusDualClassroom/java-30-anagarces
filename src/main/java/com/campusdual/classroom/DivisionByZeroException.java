73dc121d3900ef6b039950c41232764dd9a7019e
Ana Garces <anagarcestech@gmail.com>
2025-02-04 13:23:27 +0100

    ejercicio 30 resuelto

---
+++ src/main/java/com/campusdual/classroom/DivisionByZeroException.java
@@ -0,0 +1,12 @@
+package com.campusdual.classroom;
+
+public class DivisionByZeroException extends ArithmeticException {
+
+    public DivisionByZeroException(){
+        super();
+    }
+
+    public DivisionByZeroException(String s){
+        super(s);
+    }
+}
--- src/main/java/com/campusdual/classroom/Exercise30.java
+++ src/main/java/com/campusdual/classroom/Exercise30.java
@@ -2,11 +2,26 @@ package com.campusdual.classroom;
 
 public class Exercise30 {
 
-    public static int divisionWithCustomException(int dividend, int divisor){
+    public static int divisionWithCustomException(int dividend, int divisor) throws DivisionByZeroException{
+
+        if(divisor == 0){
+            throw new DivisionByZeroException("No se puede dividir un numero entre 0");
+        } else{
+            return dividend / divisor;
+        }
 
     }
 
     public static void main(String[] args) {
+
+        try{
+            divisionWithCustomException(3, 0);
+        } catch (DivisionByZeroException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Programa terminado");
+
+
 
     }
 }
